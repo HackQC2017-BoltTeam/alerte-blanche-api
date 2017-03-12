d332c03826b86883157793b06384d6077f567bea
Francis Bergin <bergin.francis@gmail.com>
2017-03-12 01:11:52 -0500

    Add more cases

--- tests.py
+++ tests.py
@@ -11,6 +11,18 @@ class NormalizeLicensePlateTest(unittest.TestCase):
     def test_with_hyphens(self):
         self.assertEqual('ABC123', normalize_plate_number('ABC-_-123'))
 
+    def test_with_lower_and_spaces(self):
+        self.assertEqual('ABC123', normalize_plate_number('abc 123'))
+
+    def test_with_lower_and_hyphens(self):
+        self.assertEqual('ABC123', normalize_plate_number('abc-123'))
+
+    def test_with_mixed_case_and_spaces(self):
+        self.assertEqual('ABC123', normalize_plate_number('AbC 123'))
+
+    def test_with_mixed_case_and_hyphens(self):
+        self.assertEqual('ABC123', normalize_plate_number('AbC-123'))
+
     def test_case_sensitivity(self):
         self.assertEqual(normalize_plate_number('abc123'),
                          normalize_plate_number('ABC123'))
